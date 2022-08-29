2e8b6da3251c835c50ea9be84105f63c44981bf4
Ruge Li <rugeli0605@gmail.com>
2022-08-29 12:45:40 -0700

    add Ruge

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 #adding three names to the new employee array
-employees = ['Nivedita', 'Na Hyung', 'Saurabh']
+employees = ['Nivedita', 'Na Hyung', 'Saurabh', 'Ruge']
 
 for employee in employees:
   print(employee)
