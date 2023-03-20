7c1d6da51812716e9d0f7e6f759de61a9b66c3d2
Joe <interim17@gmail.com>
2023-03-20 16:32:26 -0700

    adding joe to employees array

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 #adding four names to the new employee array
-employees = ['Nivedita', 'Na Hyung', 'Saurabh', 'Ruge']
+employees = ['Nivedita', 'Na Hyung', 'Saurabh', 'Ruge', 'Joe']
 
 for employee in employees:
   print(employee)
